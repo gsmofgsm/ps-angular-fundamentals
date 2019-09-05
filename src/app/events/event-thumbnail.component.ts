134f454abecfaa5e65daf5285df06f2339ef52e1
Qing Ma <qing.ma@isatis-health.nl>
2019-09-05 22:26:27 +0200

    ngIf to hide

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,11 +8,11 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
         <div>Date: {{event.date}}</div>
         <div>Time: {{event.time}}</div>
         <div>Price: \${{event.price}}</div>
-        <div>
+        <div *ngIf="event.location">
             <span>Location: {{event.location?.address}}</span>
             <span class="pad-left">{{event.location?.city}}, {{event.location?.country}}</span>
         </div>
-        <div>Url: {{event.onlineUrl}}</div>
+        <div *ngIf="event.onlineUrl">Url: {{event.onlineUrl}}</div>
         <button class="btn btn-primary" (click)="clickMe(event.name)">Click Me!</button>
     </div>
     `,
