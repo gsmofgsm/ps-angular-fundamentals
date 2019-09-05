c7a9303d034519a0f8f72bb042856a0ed69240f7
Qing Ma <qing.ma@isatis-health.nl>
2019-09-05 22:52:41 +0200

    conditionally add 2 classes by ngClass with function

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
     <div class="well hoverwell thumbnail">
         <h2>{{event.name}}</h2>
         <div>Date: {{event.date}}</div>
-        <div [ngClass]="{green: event.time === '8:00 am', bold: event.time === '8:00 am'}" [ngSwitch]="event.time">
+        <div [ngClass]="getStartTimeClass()" [ngSwitch]="event.time">
             Time: {{event.time}}
             <span *ngSwitchCase="'8:00 am'"> (Early start)</span>
             <span *ngSwitchCase="'10:00 am'"> (Late start)</span>
@@ -34,5 +34,10 @@ export class EventThumbnailComponent {
 
     clickMe(data) {
         this.eventClick.emit(data);
+    }
+
+    getStartTimeClass() {
+        const earlyStart = this.event && this.event.time === '8:00 am';
+        return {green: earlyStart, bold: earlyStart};
     }
 }
