289d8b4c5c320eb5ef8b115b982a388d8bdd1364
Qing Ma <qing.ma@isatis-health.nl>
2019-09-05 23:11:39 +0200

    getEvents is not responsibility of EventListComponent, so put it into EventService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { EventListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavbarComponent } from './nav/navbar.component'
 
+import { EventService } from './events/shared/event.service';
+
 @NgModule({
   imports: [
     BrowserModule
@@ -16,6 +18,7 @@ import { NavbarComponent } from './nav/navbar.component'
     EventThumbnailComponent,
     NavbarComponent
   ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
