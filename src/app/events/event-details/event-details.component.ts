5e44c2e7fe63a4daf075576f55d8f46ab4bceda2
Qing Ma <qing.ma@isatis-health.nl>
2019-09-11 23:00:12 +0200

    navigate to add session page by clicking add session on event details page

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from '../shared/event.service'
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
@@ -8,18 +8,23 @@ import { ActivatedRoute } from '@angular/router';
     styles: [`
         .container { padding-left: 20px; padding-right: 20px;}
         .event-image { max-height: 100px; }
+        a {cursor:pointer}
     `]
 })
 export class EventDetailsComponent implements OnInit {
 
     event:any
 
-    constructor(private eventService: EventService, private route: ActivatedRoute) {
+    constructor(private eventService: EventService, private route: ActivatedRoute, private router:Router) {
 
     }
 
     ngOnInit() {
         this.event = this.eventService.getEvent
             (+this.route.snapshot.params['id'])
+    }
+
+    addSession() {
+        this.router.navigate(['/events/sessions/new'])
     }
 }
