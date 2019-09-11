8a4f5a3b93c6542a2cb81711ecbc483061f6176b
Qing Ma <qing.ma@isatis-health.nl>
2019-09-11 22:28:49 +0200

    validate restricted word foo

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -27,7 +27,7 @@ export class CreateSessionComponent implements OnInit {
         this.presenter = new FormControl('', Validators.required),
         this.duration = new FormControl('', Validators.required),
         this.level = new FormControl('', Validators.required),
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(4)])
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(4), this.restrictedWords])
 
         this.newSessionForm = new FormGroup({
             name: this.name,
@@ -36,6 +36,12 @@ export class CreateSessionComponent implements OnInit {
             level: this.level,
             abstract: this.abstract,
         })
+    }
+
+    private restrictedWords(control: FormControl):{[key:string]:any} {
+        return control.value.includes('foo') 
+            ? {'restrictedWords': 'foo'}
+            : null
     }
 
     saveSession(formValues) {
