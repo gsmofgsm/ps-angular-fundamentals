966972681ec9f324f35cc6c7e211cb317c225a27
Qing Ma <qing.ma@isatis-health.nl>
2019-09-11 16:25:18 +0200

    error message for validting firstName lastName

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
     templateUrl: './profile.component.html',
     styles: [`
-        em  {float:right;color#E05C65;padding-left:10px}
+        em  {float:right;color:#E05C65;padding-left:10px}
         .error input {background-color:#E3C3C5;}
         .error ::-webkit-input-placeholder {color:#999;}
         .error ::-moz-placeholder {color:#999;}
