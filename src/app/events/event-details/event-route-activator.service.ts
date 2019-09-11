49c98ca190f157b208e11980f0b5479b2fbed44c
Qing Ma <qing.ma@isatis-health.nl>
2019-09-11 11:06:15 +0200

    Revised: interface CanDeactive can not be implemented, just adding the function will do

--- src/app/events/event-details/event-route-activator.service.ts
+++ src/app/events/event-details/event-route-activator.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core'
-import { CanActivate, ActivatedRouteSnapshot, Router, CanDeactivate } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { EventService } from '../shared/event.service';
 import { CreateEventComponent } from '../create-event.component';
 
 @Injectable()
-export class EventRouteActivator implements CanActivate, CanDeactivate {
+export class EventRouteActivator implements CanActivate {
      constructor(private eventService: EventService, private router: Router) {
 
      }
@@ -16,7 +16,7 @@ export class EventRouteActivator implements CanActivate, CanDeactivate {
         return eventExists
      }
 
-     canDeactivate(component: CreateEventComponent) {
+     canDeactivate(component: CreateEventComponent):boolean {
         if (component.isDirty) {
             return window.confirm('Are you sure to leave this page?')
         }
