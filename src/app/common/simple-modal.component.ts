7d0634dbe4bdb280ef6a63f69a97df4cc4757688
Qing Ma <qing.ma@isatis-health.nl>
2019-09-14 00:08:41 +0200

    configurable closeOnBodyClick of the modal

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -26,6 +26,7 @@ import { JQ_TOKEN } from './jQuery.service';
 export class SimpleModalComponent {
     @Input() title:string
     @Input() modalId:string
+    @Input() closeOnBodyClick:string
     @ViewChild('modalcontainer') containerEl:ElementRef
 
     constructor(@Inject(JQ_TOKEN) private $:any) {
@@ -33,6 +34,8 @@ export class SimpleModalComponent {
     }
 
     closeModal() {
-        this.$(this.containerEl.nativeElement).modal('hide')
+        if(this.closeOnBodyClick === 'true') {
+            this.$(this.containerEl.nativeElement).modal('hide')
+        }
     }
 }
