937575b74d6380c3faa27def7a2e8b17e79dfec4
Qing Ma <qing.ma@isatis-health.nl>
2019-09-15 21:54:06 +0200

    delete voter from http server

--- src/app/events/event-details/voter.service.ts
+++ src/app/events/event-details/voter.service.ts
@@ -8,11 +8,15 @@ import { catchError } from 'rxjs/operators';
 export class VoterService {
     constructor(private http:HttpClient){}
 
-    deleteVoter(session:ISession, voterName:string) {
+    deleteVoter(eventId:number, session:ISession, voterName:string) {
         session.voters = session.voters.filter(voter => voter !== voterName)
+        const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`
+        this.http.delete(url).pipe(catchError(this.handleError('deleteVoter'))).subscribe()
     }
 
     addVoter(eventId:number, session:ISession, voterName:string) {
+        session.voters.push(voterName)
+        
         const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`
         const options = {headers: new HttpHeaders({'Content-Type': 'application/json'})}
         this.http.post(url, {}, options).pipe(catchError(this.handleError('addVoter'))).subscribe()
