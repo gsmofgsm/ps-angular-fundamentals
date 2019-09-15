1a951590fff003d362a2f9896d221ac433ffd253
Qing Ma <qing.ma@isatis-health.nl>
2019-09-15 22:19:00 +0200

    authenticate from http server

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { IUser } from './user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable()
 export class AuthService {
     currentUser:IUser
 
+    constructor(private http:HttpClient){}
+
     loginUser(userName: string, password: string) {
-        this.currentUser = {
-            id: 1,
-            userName: userName,
-            firstName: 'GsM',
-            lastName: 'Papa'
-        }
+        const loginInfo = {username: userName, password: password}
+        const options = {headers: new HttpHeaders({'Content-Type': 'application/json'})}
+        return this.http.post('/api/login', loginInfo, options)
+            .pipe(tap(data => {
+                this.currentUser = <IUser>data['user']
+            }))
+            .pipe(catchError(error => {
+                return of(false)
+            }))
     }
 
     updateCurrentUser(firstName:string, lastName:string) {
