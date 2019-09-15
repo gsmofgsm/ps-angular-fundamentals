f2fa3f7848a8363b5ad626ce0d03366608212bcf
Qing Ma <qing.ma@isatis-health.nl>
2019-09-15 22:36:28 +0200

    logout

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -42,4 +42,10 @@ export class AuthService {
                 }
             })
     }
+
+    logout() {
+        this.currentUser = undefined
+        const options = {headers: new HttpHeaders({'Content-Type': 'application/json'})}
+        return this.http.post('/api/logout', {}, options)
+    }
 }
