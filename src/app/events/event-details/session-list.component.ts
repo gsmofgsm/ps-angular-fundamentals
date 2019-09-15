7aaf115d123cae74e6d91f7775e33c5fd8f55855
Qing Ma <qing.ma@isatis-health.nl>
2019-09-15 21:45:09 +0200

    add voter to http server

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -11,6 +11,7 @@ export class SessionListComponent implements OnChanges {
     @Input() sessions:ISession[]
     @Input() filterBy:string
     @Input() sortBy:string
+    @Input() eventId:number
     visibleSessions:ISession[]
 
     constructor(private auth:AuthService, private voterService:VoterService) {
@@ -28,7 +29,7 @@ export class SessionListComponent implements OnChanges {
         if (this.userHasVoted(session)) {
             this.voterService.deleteVoter(session, this.auth.currentUser.userName)
         } else {
-            this.voterService.addVoter(session, this.auth.currentUser.userName)
+            this.voterService.addVoter(this.eventId, session, this.auth.currentUser.userName)
         }
 
         if (this.sortBy === 'votes') {
--- src/app/events/event-details/voter.service.ts
+++ src/app/events/event-details/voter.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@angular/core';
 import { ISession } from '../shared';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class VoterService {
+    constructor(private http:HttpClient){}
+
     deleteVoter(session:ISession, voterName:string) {
         session.voters = session.voters.filter(voter => voter !== voterName)
     }
 
-    addVoter(session:ISession, voterName:string) {
-        session.voters.push(voterName)
+    addVoter(eventId:number, session:ISession, voterName:string) {
+        const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`
+        const options = {headers: new HttpHeaders({'Content-Type': 'application/json'})}
+        this.http.post(url, {}, options).pipe(catchError(this.handleError('addVoter'))).subscribe()
     }
 
     userHasVoted(session:ISession, voterName:string) {
         return session.voters.some(voter => voter === voterName)
+    }
+
+    private handleError<T> (operation = 'operation', result?:T) {
+      return (error:any):Observable<T> => {
+        console.error(error)
+        return of(result as T)
+      }
     }
 }
