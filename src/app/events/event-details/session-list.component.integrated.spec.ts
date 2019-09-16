749afe57dab4b5b3d23901654ce7f5d95e33d437
Qing Ma <qing.ma@isatis-health.nl>
2019-09-16 22:52:17 +0200

    set up integrated test for SessionListComponent

---
+++ src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { SessionListComponent } from './session-list.component';
+import { DebugElement } from '@angular/core';
+import { async } from 'q';
+import { AuthService } from 'src/app/user/auth.service';
+import { VoterService } from './voter.service';
+
+describe('SessionListComponent', () => {
+    let fixture:ComponentFixture<SessionListComponent>,
+        component: SessionListComponent,
+        element: HTMLElement,
+        debugEl: DebugElement
+
+    beforeEach(async(() => {
+        let mockAuthService = {}
+        let mockVoterService = {}
+
+        TestBed.configureTestingModule({
+            imports: [],
+            declarations: [
+                SessionListComponent
+            ],
+            providers: [
+                {provide: AuthService, useValue: mockAuthService},
+                {provide: VoterService, useValue: mockVoterService},
+            ],
+            schemas: []
+        })
+    }))
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SessionListComponent)
+        component = fixture.componentInstance
+        debugEl = fixture.debugElement
+        element = fixture.nativeElement
+    })
+})
